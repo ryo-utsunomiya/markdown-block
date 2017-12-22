df68f641bdb587c49227bdd349738bd950d34ed2
Ryo Utsunomiya <millionsnowliving@gmail.com>
2017-12-22 14:43:37 +0900

    Add linter

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-    entry: './index.js',
-    output: {
-        path: __dirname,
-        filename: 'index.build.js',
-    },
-    module: {
-        loaders: [
-            {
-                test: /.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
+	entry: './index.js',
+	output: {
+		path: __dirname,
+		filename: 'index.build.js',
+	},
+	module: {
+		loaders: [
+			{
+				test: /.js$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/,
+			},
+		],
+	},
 };
